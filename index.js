40e69012b24bbcf865218e7e9514365b0633b565
Rahilka Simonova <rahilka.simonova@mi-c3.com>
2017-03-10 20:54:48 +0100

    Extracted options

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var map;
+var element = document.getElementById('map');
+var options = {
+                center: {lat: 41.997346, lng: 21.427996},
+                zoom: 8,
+                disableDefaultUI: true,
+                scrollwheel: true,
+                draggable: true
+            };
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41.997346, lng: 21.427996},
-        zoom: 8
-    });
+    map = new google.maps.Map(element, options);
+
+    map.setZoom(18);
+
 }
